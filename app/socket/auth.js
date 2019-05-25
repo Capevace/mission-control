fc365a4313d54dee51888ea3134176e626a30f74
Lukas Mateffy <capevace@gmail.com>
2019-05-26 00:49:09 +1000

    Added improved socket authentication

---
+++ app/socket/auth.js
@@ -0,0 +1,86 @@
+const config = require('@config');
+const log = require('@helpers/log').logger('Socket Auth');
+const jwt = require('jsonwebtoken');
+
+module.exports = function socketAuth(socketIO, onAuthentication) {
+	forbidNamespaceConnections(socketIO.nsps);
+
+	socketIO.on('connection', socket => {
+		// Set the timeout after which the socket will be disconnected
+		// if no authentication is reached before that.
+		const authTimeoutId = setTimeout(
+			() =>
+				socket.emit(
+					'unauthorized',
+					{
+						type: 'TIMEOUT'
+					},
+					() => socket.disconnect()
+				),
+			config.auth.socketTimeout
+		);
+		socket.authenticated = false;
+
+		socket.on('authenticate', ({ token }) => {
+			clearTimeout(authTimeoutId);
+
+			try {
+				// Verify JWT received with the secret
+				const payload = jwt.verify(token, config.auth.jwtSecret, {
+					issuer: config.auth.issuer,
+					audience: config.auth.audience
+				});
+
+				socket.jwt = token;
+				socket.jwtPayload = payload;
+
+				// AUTH SUCCESSFUL
+				// We set the authenticated var in the socket instance to true and
+				// restore any connections the socket tried to make to namespaces.
+				socket.authenticated = true;
+				onAuthentication(socket);
+				restoreNamespaceConnection(socketIO.nsps, socket);
+
+				socket.emit('authenticated');
+			} catch (e) {
+				log('Client couldnt be authorized:', e.message);
+
+				// Send the unauthorized event and disconnect
+				socket.emit(
+					'unauthorized',
+					{
+						type: 'INVALID_TOKEN'
+					},
+					() => socket.disconnect()
+				);
+			}
+		});
+	});
+};
+
+function forbidNamespaceConnections(namespaces) {
+	Object.values(namespaces).forEach(namespace => {
+		namespace.on('connect', socket => {
+			// If the socket isnt authenticated, we remove it from the connected list.
+			// Now it wont get any broadcasts.
+			if (!socket.authenticated) {
+				delete namespace.connected[socket.id];
+			}
+		});
+	});
+}
+
+function restoreNamespaceConnection(namespaces, socket) {
+	Object.values(namespaces).forEach(namespace => {
+		// Go through all sockets in the namespace and see if our socket is in there.
+		// If so, we restore the connection.
+		if (
+			Object.values(namespace.sockets).reduce(
+				(found, s) => found || socket.id === s.id,
+				false
+			)
+		) {
+			namespace.connected[socket.id] = socket;
+		}
+	});
+}
